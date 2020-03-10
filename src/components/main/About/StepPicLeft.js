610d40c3b275779993590de660d815f673cd88a2
Ilya Soloveychik <dj.active.17@gmail.com>
2020-03-10 12:29:03 +0200

    change steps component

--- src/components/main/About/StepPicLeft.js
+++ src/components/main/About/StepPicLeft.js
@@ -1,23 +1,21 @@
 import React from 'react';
 
-
-
-class Step extends React.Component {
+class StepPicLeft extends React.Component {
 
      render() {
         return (
             <div className="row">
-                <div className="main_step step-pic-left col-sm-6">
+                <div className="main_step col-sm-6">
+                    <img src={this.props.img} alt={`step-${this.props.number}`} className="main_step-pic"/>
+                </div>
+                <div className="main_step col-sm-6">
                     <h3 className="main_step-number">Step {this.props.number}</h3>
                     <h4 className="main_step-title">{this.props.title}</h4>
                     <p className="main_step-description">{this.props.description}</p>
-                </div>
-                <div className="main_step step-pic-right col-sm-6">
-                    <img src={this.props.img} alt={`step-${this.props.number}`} className="main_step-pic"/>
                 </div>
             </div>
         )
     }
 }
 
-export default Step;
+export default StepPicLeft;
---
+++ src/components/main/About/StepPicRight.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class StepPicRight extends React.Component {
+
+    render() {
+        return (
+            <div className="row">
+                <div className="main_step col-sm-6">
+                    <h3 className="main_step-number">Step {this.props.number}</h3>
+                    <h4 className="main_step-title">{this.props.title}</h4>
+                    <p className="main_step-description">{this.props.description}</p>
+                </div>
+                <div className="main_step col-sm-6">
+                    <img src={this.props.img} alt={`step-${this.props.number}`} className="main_step-pic"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default StepPicRight;
