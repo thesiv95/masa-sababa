f8da6fe980ffcdc34e63d857069cc0812d7a0f9d
Ilya Soloveychik <dj.active.17@gmail.com>
2020-03-11 10:40:40 +0200

    fix signup

--- src/components/header/SignUp.js
+++ src/components/header/SignUp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import {NavLink, Route} from "react-router-dom";
 import Lang from "./../../i18n/ru";
 
 class SignUp extends React.Component {
@@ -10,25 +10,6 @@ class SignUp extends React.Component {
                     <NavLink to='/profile'>{Lang.header_btn_signup}</NavLink>
                 </button>
             </div>
-
-
-
-
-            /*
-            <div className="header_signup col-md-1">
-                <ul className="header_nav-list">
-                    <li className="header_nav-item">
-                        <NavLink to='/profile' className='header_link' activeClassName='nav-active'>{Lang.header_menu_profile}</NavLink>
-                    </li>
-                    <li className="header_nav-item">
-                        <NavLink to='/home' className='header_link' activeClassName='nav-active'>{Lang.header_menu_logout}</NavLink>
-                    </li>
-                </ul>
-            </div>
-
-
-             */
-
         )
     }
 }
