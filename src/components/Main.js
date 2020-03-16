c85cab7ae94bac0617aae31efdce21236fa1ea01
Ilya Soloveychik <dj.active.17@gmail.com>
2020-03-16 13:39:57 +0200

    fix notfound page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Home from "./main/Home";
 import About from "./main/About";
 import Blog from "./main/Blog";
@@ -13,14 +13,16 @@ class Main extends React.Component {
         let d = new Date();
         return (
             <main className='main'>
-                <Route path='/' exact component={Home}/>
-                <Route path='/about' component={About}/>
-                <Route path='/blog' component={Blog}/>
-                <Route path='/profile' component={Profile}/>
-                <Route path='/fullarticle' render={() =>
-                    <FullArticle author={'Moshe Dayan'} day={d.getDate()} month={d.getMonth()} year={d.getFullYear()} />
-                }/>
-                <Route path='*' component={NotFound404}/>
+                <Switch>
+                    <Route path='/' exact component={Home}/>
+                    <Route path='/about' component={About}/>
+                    <Route path='/blog' component={Blog}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/fullarticle' render={() =>
+                        <FullArticle author={'Moshe Dayan'} day={d.getDate()} month={d.getMonth()} year={d.getFullYear()} />
+                    }/>
+                    <Route component={NotFound404}/>
+                </Switch>
             </main>
         )
     }
--- src/components/main/NotFound404.js
+++ src/components/main/NotFound404.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Lang from "../../i18n/lang";
+import languageSet from "../../utilites/languageSet";
+import {NavLink} from "react-router-dom";
 
 class NotFound404 extends React.Component {
     render() {
+        let displayLanguage = languageSet();
         return (
             <div>
-                <h2 className='main_header'>Sorry! Page not found</h2>
+                <h2 className='main_header'>{Lang[displayLanguage].main_404_header}</h2>
                 <div className="main_steps container">
                     <div className="row">
                         <div className="col-sm-offset-5">
-                            <a className="button button-accent" href='/home'>Go Home</a>
+                            <NavLink to={'/'} className={'button button-accent'}>Go Home</NavLink>
                         </div>
                     </div>
 
