b6e053613c98be44b627958b59d22bcd99413091
Ilya Soloveychik <dj.active.17@gmail.com>
2020-03-22 16:40:47 +0200

    update anchor styles

--- src/components/main/About/Useful.js
+++ src/components/main/About/Useful.js
@@ -8,7 +8,7 @@ class Useful extends React.Component {
                 <img src={this.props.imgSrc} alt={this.props.imgAlt} className="main_articles-article-userpic"/>
                 <h4 className="main_articles-article-title">{this.props.title}</h4>
                 <p className="main_articles-article-description">
-                    <a href={this.props.url} target="_blank" rel="noopener noreferrers">{this.props.description}</a>
+                    <a href={this.props.url} target="_blank" rel="noopener noreferrer">{this.props.description}</a>
                 </p>
             </div>
         )
--- src/components/main/Blog/FullArticle/LeaveReply.js
+++ src/components/main/Blog/FullArticle/LeaveReply.js
@@ -31,6 +31,7 @@ class LeaveReply extends React.Component {
         }
     };
 
+
     addComment = () => {
         let replyText = document.querySelector('#replyText'),
             replyName = document.querySelector('#replyName'),
@@ -59,7 +60,8 @@ class LeaveReply extends React.Component {
             <div className='main_leavereply'>
                     <div className='main_leavereply_message'>
                     </div>
-                    <a name='reply'/>
+
+                    <a name='reply' className='main_anchor' />
                     <h3>{Lang[displayLanguage].reply_title}</h3>
                     <textarea id="replyText" placeholder={Lang[displayLanguage].reply_placeholder_yourtext} className='main_leavereply-textarea' />
                     <div className="main_leavereply-inputs">
