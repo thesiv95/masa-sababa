a3ee25e2c49d3c8f8a476e52683ba7a229ef1a10
Ilya Soloveychik <dj.active.17@gmail.com>
2020-03-23 18:03:23 +0200

    update Search.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,17 @@ import Nav from "./header/Nav";
 import Search from "./header/Search";
 import Signed from "./header/Signed";
 import SignUp from "./header/SignUp";
+import data from "./header/dataSearch"; // all search queries for all languages
+import languageSet from "../utilites/languageSet";
+
+// the single string will be splitted by '/' (title/url) because it is too hard to pass big objects :/
+// idea: https://youtu.be/NnpISZANByg
+// search items will be passed through props
 
 
 class Header extends React.Component {
+
+    displayLanguage = languageSet();
 
     constructor(props) {
         super(props);
@@ -15,9 +23,7 @@ class Header extends React.Component {
         }
     }
 
-
-
-    checkToken = () => {
+   checkToken = () => {
         if (localStorage.getItem('token') !== null && localStorage.getItem('currentUser') !== null){
             this.setState({isLogged: true});
         } else {
@@ -35,7 +41,7 @@ class Header extends React.Component {
                     <div className="row">
                         <Logo/>
                         <Nav/>
-                        <Search/>
+                        <Search items={data[this.displayLanguage]}/>
                         {
                             this.state.isLogged
                                 ? <Signed/>
