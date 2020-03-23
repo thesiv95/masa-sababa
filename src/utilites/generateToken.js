6c7739557a139c303e69a51849f1a7e571731a9d
Ilya Soloveychik <dj.active.17@gmail.com>
2020-03-23 21:51:05 +0200

    clean master branch

--- src/utilites/generateToken.js
+++
@@ -1,8 +0,0 @@
-// We don't use passwords for 'leave a reply page', but we use them for 'profile' page
-
-const generateToken = (email, password = '') => {
-    let str = (password !== '') ? `${email}:${password}` : email;
-    return window.btoa(unescape(encodeURIComponent(str))); // MDN website
-}
-
-export default generateToken;
